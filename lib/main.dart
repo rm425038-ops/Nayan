44ca9ca9af6f0ebbfad618a40d153ca668886a14
rm425038-ops <rm425038@gmail.com>
2026-03-31 17:43:34 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,59 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(NayanApp());
 }
 
-class MyApp extends StatelessWidget {
+class NayanApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Nayan App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Nayan'),
-        ),
-        body: Center(
-          child: Text('Hello, Nayan!'),
-        ),
+      debugShowCheckedModeBanner: false,
+      home: VideoScreen(),
+    );
+  }
+}
+
+class VideoScreen extends StatelessWidget {
+  final List<String> videos = [
+    "🔥 Video 1",
+    "😎 Video 2",
+    "🚀 Video 3",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: videos.length,
+        itemBuilder: (context, index) {
+          return Stack(
+            children: [
+              Center(
+                child: Text(
+                  videos[index],
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 20,
+                bottom: 100,
+                child: Column(
+                  children: [
+                    Icon(Icons.favorite, color: Colors.red, size: 40),
+                    SizedBox(height: 20),
+                    Icon(Icons.comment, color: Colors.white, size: 40),
+                  ],
+                ),
+              )
+            ],
+          );
+        },
       ),
     );
   }
